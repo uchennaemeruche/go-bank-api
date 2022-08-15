155c611c637caafb73ea2d6e16ea6589166cbc22
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-15 10:23:03 +0100

    add test for createAccount, GetAccount and Update Account queries

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/uchennaemeruche/go-bank-api/util"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -25,4 +26,44 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+	return account
+}
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	res, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, res)
+
+	require.Equal(t, account.ID, res.ID)
+	require.Equal(t, account.Owner, res.Owner)
+	require.Equal(t, account.Balance, res.Balance)
+	require.Equal(t, account.Currency, res.Currency)
+	require.WithinDuration(t, account.CreatedAt, res.CreatedAt, time.Second)
+
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	res, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, res)
+
+	require.Equal(t, account.ID, res.ID)
+	require.Equal(t, account.ID, res.ID)
+	require.Equal(t, account.Owner, res.Owner)
+	require.Equal(t, arg.Balance, res.Balance)
+	require.Equal(t, account.Currency, res.Currency)
+	require.WithinDuration(t, account.CreatedAt, res.CreatedAt, time.Second)
+
 }
