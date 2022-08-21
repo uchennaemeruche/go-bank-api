d0c2f5c000fa6c885eeecac26bf4a31c1d17936a
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-22 00:13:34 +0100

    define account routes and router

---
+++ account/routes/account.router.go
@@ -0,0 +1,22 @@
+package accountRoutes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/uchennaemeruche/go-bank-api/account/handler"
+	"github.com/uchennaemeruche/go-bank-api/account/service"
+	db "github.com/uchennaemeruche/go-bank-api/db/sqlc"
+)
+
+func Init(router *gin.Engine, store *db.Store) {
+
+	r := router.Group("/accounts")
+
+	accountService := service.NewAccountService(store)
+
+	handler := handler.NewAccountHandler(accountService)
+	r.GET("/:id", handler.GetAccount)
+	r.GET("", handler.ListAccount)
+	r.POST("", handler.CreateAccount)
+	r.PUT("/:id", handler.UpdateAccount)
+	r.DELETE("/:id", handler.DeleteAccount)
+}
