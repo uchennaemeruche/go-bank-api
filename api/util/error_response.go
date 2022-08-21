12036768ed4e66a1edd63f07090b222cca1222b9
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-21 17:51:12 +0100

    add reesponse error definitions

---
+++ api/util/error_response.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+type RequestError struct {
+	Err  error
+	Code int
+}
+
+func (r *RequestError) Error() string {
+	return r.Err.Error()
+}
