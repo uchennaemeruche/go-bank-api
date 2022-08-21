8c8dd9ad56d9e2f00be0bfd80084fbba1ff35af1
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-21 17:56:22 +0100

    change PageSize binding property

--- account/entity/account.entity.go
+++ account/entity/account.entity.go
@@ -16,5 +16,5 @@ type GetAccountReq struct {
 
 type ListAccountReq struct {
 	PageId   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"requiredmin=2"`
+	PageSize int32 `form:"page_size" binding:"required,min=2"`
 }
