f05f6d8d4a4e129c64185e1a08d7f9f143a6547c
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-21 17:46:50 +0100

    add api endpints

--- api/server.go
+++ api/server.go
@@ -2,8 +2,9 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/uchennaemeruche/go-bank-api/account/handler"
+	"github.com/uchennaemeruche/go-bank-api/account/service"
 	db "github.com/uchennaemeruche/go-bank-api/db/sqlc"
-	"github.com/uchennaemeruche/go-bank-api/handler"
 )
 
 type Server struct {
@@ -18,9 +19,13 @@ func NewServer(store db.Store) *Server {
 
 	router := gin.Default()
 
-	handler := handler.NewAccountHandler()
+	accountService := service.NewAccountService(server.store)
 
-	router.GET("/accounts", handler.GetAccount)
+	handler := handler.NewAccountHandler(accountService)
+
+	router.GET("/accounts/:id", handler.GetAccount)
+	router.GET("/accounts", handler.CreateAccount)
+	router.POST("/accounts", handler.CreateAccount)
 
 	server.router = router
 
