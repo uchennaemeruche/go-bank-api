a48ab3866031ad70a0ecfcf1035daa1a331f4d6d
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-23 08:05:27 +0100

    register currency validator

--- api/server.go
+++ api/server.go
@@ -2,7 +2,11 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	accountRoutes "github.com/uchennaemeruche/go-bank-api/account/routes"
+	transferRoutes "github.com/uchennaemeruche/go-bank-api/transfer/routes"
+
 	db "github.com/uchennaemeruche/go-bank-api/db/sqlc"
 )
 
@@ -15,10 +19,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{
 		store: store,
 	}
-
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	accountRoutes.Init(router, store)
+
+	transferRoutes.Init(router, store)
 
 	server.Router = router
 
