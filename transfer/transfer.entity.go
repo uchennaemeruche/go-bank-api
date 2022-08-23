fe414ff985617f3925ed11b7e837c3c6061d7f8e
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-23 08:06:52 +0100

    add transfer api entity

---
+++ transfer/transfer.entity.go
@@ -0,0 +1,8 @@
+package transfer
+
+type TransferReq struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
+}
