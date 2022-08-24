a71bbc8646127a70beb63391b69729895f8b1e20
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-24 02:56:27 +0100

    add a HashPassword util function to create a Hashed password

---
+++ util/password.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "nil", fmt.Errorf("failed to hash password %s", err)
+	}
+	return string(hashedPassword), nil
+}
+
+func ComparePassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
