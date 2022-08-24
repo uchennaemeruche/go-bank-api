3d9761ed2cf97cf8e84441633a0f0e2fa2d461c6
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-24 02:55:53 +0100

    update user test to use Hashed Password

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "sectet",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
