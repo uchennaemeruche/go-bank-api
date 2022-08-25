ffc12d3329e470cc9c8540e0e6a74978a5d6d93e
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-25 14:07:30 +0100

    add config and tokenMaker to server params

--- api/server.go
+++ api/server.go
@@ -1,25 +1,38 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	accountRoutes "github.com/uchennaemeruche/go-bank-api/account/routes"
 	api "github.com/uchennaemeruche/go-bank-api/api/util"
+	"github.com/uchennaemeruche/go-bank-api/token"
 	transferRoutes "github.com/uchennaemeruche/go-bank-api/transfer/routes"
 	userRoutes "github.com/uchennaemeruche/go-bank-api/user/routes"
+	"github.com/uchennaemeruche/go-bank-api/util"
 
 	db "github.com/uchennaemeruche/go-bank-api/db/sqlc"
 )
 
 type Server struct {
-	store  db.Store
-	Router *gin.Engine
+	config     util.Config
+	tokenMaker token.Maker
+	store      db.Store
+	Router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create a token maker: %w", err)
+	}
+
 	server := &Server{
-		store: store,
+		store:      store,
+		config:     config,
+		tokenMaker: tokenMaker,
 	}
 	router := gin.Default()
 
@@ -31,14 +44,13 @@ func NewServer(store db.Store) *Server {
 
 	transferRoutes.Init(router, store)
 
-	userRoutes.Init(*router, store)
+	userRoutes.Init(*router, store, tokenMaker, config)
 
 	server.Router = router
 
-	return server
+	return server, nil
 }
 
 func (s *Server) Start(addr string) {
 	s.Router.Run(addr)
-
 }
