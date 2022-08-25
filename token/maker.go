608cdad6fc2d5f78ef87379c50d805fd22409355
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-25 04:41:12 +0100

    define Token maker interface

---
+++ token/maker.go
@@ -0,0 +1,14 @@
+package token
+
+import "time"
+
+/** Todo:
+* Create Token - Takes username and duration and returns a toke string or an error.
+* Verify Token - Takes a token string and returns the pointer to the Payload or an error
+**/
+
+// Defines functions that different Token providers will implement.
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (token string, err error)
+	VerifyToken(token string) (*Payload, error)
+}
