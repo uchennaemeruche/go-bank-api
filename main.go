3f49289603579bc2965a994fae3dc891a4cc5f57
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-25 14:10:20 +0100

    pass store to Server

--- main.go
+++ main.go
@@ -24,7 +24,10 @@ func main() {
 	}
 	store := db.NewStore(conn)
 
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("could not create server", err)
+	}
 
 	server.Start(config.ServerAddress)
 }
