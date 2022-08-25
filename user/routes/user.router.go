33abcc2e7baaba6c839b3e015ebe2c14900361ea
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-08-25 14:09:04 +0100

    define user login route

--- user/routes/user.router.go
+++ user/routes/user.router.go
@@ -3,15 +3,18 @@ package userRoutes
 import (
 	"github.com/gin-gonic/gin"
 	db "github.com/uchennaemeruche/go-bank-api/db/sqlc"
+	"github.com/uchennaemeruche/go-bank-api/token"
 	"github.com/uchennaemeruche/go-bank-api/user/handler"
 	"github.com/uchennaemeruche/go-bank-api/user/service"
+	"github.com/uchennaemeruche/go-bank-api/util"
 )
 
-func Init(router gin.Engine, store db.Store) {
+func Init(router gin.Engine, store db.Store, tokenMaker token.Maker, config util.Config) {
 	r := router.Group("/users")
 
-	userService := service.NewUserService(store)
-	handler := handler.NewUserHandler(userService)
+	userService := service.NewUserService(store, tokenMaker)
+	handler := handler.NewUserHandler(userService, config)
 
 	r.POST("", handler.CreateUser)
+	r.POST("/login", handler.LoginUser)
 }
